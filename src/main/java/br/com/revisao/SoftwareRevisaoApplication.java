ba9e04d1ecc4fde7fd6104c87675797efc609135
VINICIUS DHENNER MARINS DE OLIVEIRA <08217630160@ifms.edu.br>
2023-08-31 19:55:32 -0400

    first commit

---
+++ src/main/java/br/com/revisao/SoftwareRevisaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.revisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareRevisaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareRevisaoApplication.class, args);
+	}
+
+}
