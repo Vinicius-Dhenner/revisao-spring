b513a9b958601785ef30fbb153825fb2631684a9
VINICIUS DHENNER MARINS DE OLIVEIRA <08217630160@ifms.edu.br>
2023-09-12 20:29:19 -0400

    começando a dto

---
+++ src/main/java/br/com/revisao/dto/FlashcardDto.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.revisao.dto;
+
+/**
+ *
+ * @author 08217630160
+ */
+public class FlashcardDto {
+    private Long id;
+    private String front;
+    private String back;
+    private Integer term;
+    
+    public FlashcardDto(Long id, String front, String back, Integer term) {
+        this.id = id;
+        this.front = front;
+        this.back = back;
+        this.term = term;
+    } 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFront() {
+        return front;
+    }
+
+    public void setFront(String front) {
+        this.front = front;
+    }
+
+    public String getBack() {
+        return back;
+    }
+
+    public void setBack(String back) {
+        this.back = back;
+    }
+
+    public Integer getTerm() {
+        return term;
+    }
+
+    public void setTerm(Integer term) {
+        this.term = term;
+    }
+    
+}
