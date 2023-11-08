0c72bfb8feac76c312ed2e2345e0cd3ee63877db
vinicius-dhenner <vinicius-dhenner@gail.com>
2023-11-07 22:38:05 -0400

    retomando

---
+++ src/test/java/br/com/revisao/RevisaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.revisao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
