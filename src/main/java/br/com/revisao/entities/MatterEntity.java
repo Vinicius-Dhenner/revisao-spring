d467ab76ad0c6386a0353e06329ce2da624e4af7
Vinicius Dhenner <108435000+Vinicius-Dhenner@users.noreply.github.com>
2023-11-15 23:07:52 -0400

    desenvolvendo - corrigir erro postgre

---
+++ src/main/java/br/com/revisao/entities/MatterEntity.java
@@ -0,0 +1,13 @@
+package br.com.revisao.entities;
+
+import jakarta.persistence.*;
+
+@Table(name = "matters")
+@Entity(name = "matters")
+public class MatterEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
