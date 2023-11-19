77dca306e23401d18f24f6e0afb1f54a1e3e045d
Vinicius Dhenner <108435000+Vinicius-Dhenner@users.noreply.github.com>
2023-11-18 21:54:07 -0400

    Revert "desenvolvendo - corrigir erro postgre"
    This reverts commit d467ab76ad0c6386a0353e06329ce2da624e4af7.

---
+++ src/main/java/br/com/revisao/controller/FlashcardController.java
@@ -0,0 +1,9 @@
+package br.com.revisao.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //indica que eh um controller
+@RequestMapping("flashcards")
+public class FlashcardController {
+}
--- src/main/java/br/com/revisao/repositories/MatterRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.revisao.repositories;
-
-import br.com.revisao.entities.MatterEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatterRepository extends JpaRepository<MatterEntity, Long> {
-}
