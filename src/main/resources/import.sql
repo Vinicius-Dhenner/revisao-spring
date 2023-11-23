e6f1e04804bb8e80f9c2cff8592851f217aa14d4
Vinicius Dhenner <108435000+Vinicius-Dhenner@users.noreply.github.com>
2023-11-22 23:38:19 -0400

    frontend pronto

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,33 +4,30 @@ insert into matters (id, name) values (3, 'Informática Básica')
 insert into matters (id, name) values (4, 'Atualidades')
 insert into matters (id, name) values (5, 'Informática Avançada')
 
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (1, 'Front Content 1', 'Back Content 1', 1, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (2, 'Front Content 1', 'Back Content 1', 2, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (3, 'Front Content 1', 'Back Content 1', 3, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (4, 'Front Content 1', 'Back Content 1', 4, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (5, 'Front Content 1', 'Back Content 1', 5, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (6, 'Front Content 1', 'Back Content 1', 1, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (7, 'Front Content 1', 'Back Content 1', 2, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (8, 'Front Content 1', 'Back Content 1', 3, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (9, 'Front Content 1', 'Back Content 1', 4, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (10, 'Front Content 1', 'Back Content 1', 5, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (11, 'Front Content 1', 'Back Content 1', 1, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (12, 'Front Content 1', 'Back Content 1', 2, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (13, 'Front Content 1', 'Back Content 1', 3, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (14, 'Front Content 1', 'Back Content 1', 4, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (15, 'Front Content 1', 'Back Content 1', 5, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (16, 'Front Content 1', 'Back Content 1', 1, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (17, 'Front Content 1', 'Back Content 1', 2, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (18, 'Front Content 1', 'Back Content 1', 3, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (19, 'Front Content 1', 'Back Content 1', 4, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (20, 'Front Content 1', 'Back Content 1', 5, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (21, 'Front Content 1', 'Back Content 1', 1, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (22, 'Front Content 1', 'Back Content 1', 2, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (23, 'Front Content 1', 'Back Content 1', 3, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (24, 'Front Content 1', 'Back Content 1', 4, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (25, 'Front Content 1', 'Back Content 1', 5, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (26, 'Front Content 1', 'Back Content 1', 1, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (27, 'Front Content 1', 'Back Content 1', 2, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (28, 'Front Content 1', 'Back Content 1', 3, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (29, 'Front Content 1', 'Back Content 1', 4, '2023-11-19', 0)
-insert into flashcards (id, front, back, id_matter, dateshow, daysshow) values (30, 'Front Content 1', 'Back Content 1', 5, '2023-11-19', 0)
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (1, 'Qual é a capital do Japão?', 'A resposta correta é Tóquio.', 1, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (2, 'Quem foi o presidente dos Estados Unidos durante a Guerra Civil Americana?', 'A resposta correta é Abraham Lincoln.', 2, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (3, 'O que é a Teoria da Relatividade?', 'A resposta correta é uma teoria física proposta por Albert Einstein.', 3, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (4, 'Quem foi o autor de "Cem Anos de Solidão"?', 'A resposta correta é Gabriel García Márquez.', 4, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (5, 'Qual é a principal função do sistema nervoso central?', 'A resposta correta é controlar e coordenar as funções do corpo.', 5, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (6, 'O que é a equação de Schroedinger?', 'A resposta correta é uma equação fundamental na teoria quântica.', 1, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (7, 'Quem foi o líder da Revolução Cubana?', 'A resposta correta é Fidel Castro.', 2, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (8, 'Qual é a importância da Declaração Universal dos Direitos Humanos?', 'A resposta correta é estabelecer os direitos fundamentais para todos os seres humanos.', 3, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (9, 'O que é a teoria do Big Bang?', 'A resposta correta é a teoria científica sobre a origem do universo.', 4, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (10, 'Quem foi Marie Curie?', 'A resposta correta é uma renomada cientista ganhadora de dois prêmios Nobel.', 5, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (11, 'O que é a teoria da evolução de Darwin?', 'A resposta correta é a explicação científica para a diversidade das espécies.', 1, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (12, 'Quem foi Mahatma Gandhi?', 'A resposta correta é um líder pacifista e defensor dos direitos civis na Índia.', 2, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (13, 'O que é a teoria da gravidade?', 'A resposta correta é a teoria proposta por Isaac Newton para explicar a atração entre corpos massivos.', 3, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (14, 'Quem foi Leonardo da Vinci?', 'A resposta correta é um renascentista italiano conhecido por suas contribuições nas artes e ciências.', 4, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (15, 'Qual é a diferença entre mitose e meiose?', 'A resposta correta é que a mitose resulta em células geneticamente idênticas, enquanto a meiose produz células geneticamente diferentes.', 5, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (16, 'Quem foi Nelson Mandela?', 'A resposta correta é um líder anti-apartheid e ex-presidente da África do Sul.', 1, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (17, 'O que é a teoria das cordas?', 'A resposta correta é uma teoria física que busca explicar a natureza fundamental das partículas e das forças.', 2, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (18, 'Quem foi Ada Lovelace?', 'A resposta correta é considerada a primeira programadora de computadores.', 3, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (19, 'O que é a Lei de Moore?', 'A resposta correta é a observação de que o número de transistores em um chip de computador dobra aproximadamente a cada dois anos.', 4, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (20, 'Quem foi Martin Luther King Jr.?', 'A resposta correta é um líder dos direitos civis nos Estados Unidos.', 5, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (21, 'Qual é a diferença entre uma célula procariótica e uma eucariótica?', 'A resposta correta é que as células procarióticas não têm núcleo definido, enquanto as células eucarióticas têm.', 1, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (22, 'O que é a teoria do aquecimento global?', 'A resposta correta é a teoria que sugere que as atividades humanas estão contribuindo para o aumento da temperatura média da Terra.', 2, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (23, 'Quem foi Alan Turing?', 'A resposta correta é um pioneiro da ciência da computação e um decodificador-chefe durante a Segunda Guerra Mundial.', 3, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (24, 'O que é a teoria da seleção natural?', 'A resposta correta é a ideia de Charles Darwin de que as características hereditárias favoráveis são transmitidas às gerações subsequentes.', 4, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (25, 'Quem foi Rosalind Franklin?', 'A resposta correta é uma química cujo trabalho foi fundamental para a compreensão da estrutura do DNA.', 5, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (26, 'Qual é o impacto da inteligência artificial na sociedade?', 'A resposta correta é uma influência significativa nas áreas de trabalho, ética e privacidade.', 1, '2023-11-19', 0);
+INSERT INTO flashcards (id, front, back, id_matter, dateshow, daysshow) VALUES (27, 'O que é a teoria do caos?', 'A resposta correta é um campo de estudo em matemática e física que trata de sistemas extremamente sensíveis às condições iniciais.', 2, '2023-11-19', 0);
